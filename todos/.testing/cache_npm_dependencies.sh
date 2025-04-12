13a7cbebd7d2a2358c0a8ae349120fb35308f623
scaperow <scaperow@hotmail.com>
2025-04-12 20:48:11 +0800

    init

--- todos/.testing/cache_npm_dependencies.sh
+++
@@ -1,4 +0,0 @@
-# Cache npm deps
-if [ ! -e /opt/circleci/nodejs/v8.9.3/lib/node_modules/chimp/bin/chimp.js ]; then npm install -g chimp; fi
-if [ ! -e /opt/circleci/nodejs/v8.9.3/lib/node_modules/spacejam/bin/spacejam ]; then npm install -g spacejam; fi
-npm install
